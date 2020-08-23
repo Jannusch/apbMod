bfa99a8acecb2a4763db6a74424d9a4f051443d0
Jannusch Bigge <jannusch.bigge@protonmail.com>
2020-08-23 19:39:39 +0200

    its soooooooooooooo bright

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-minetest.register_node("apbMod:glowing_Floor", {
+minetest.register_node("apbmod:glowing_Floor", {
 	description = "This floor is so shiny",
 	tiles = {"default_brick.png"},
 	paramtype = "light",
-	light_source = 4,
-}
+	light_source = 15,
+})
 
 
