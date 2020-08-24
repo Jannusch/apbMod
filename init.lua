948b96243f46ff09e50d1a6fe8b79a01cdaf06e3
Jannusch <jannusch.bigge@protonmail.com>
2020-08-24 10:17:48 +0200

    blind strips with 2 strips

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ minetest.register_node("apbmod:normal_Floor", {
 	groups = { cracky = 3},
 })
 
-minetest.register_node("apbmod:guide_strips_for_the_blind", {
+minetest.register_node("apbmod:guide_strips_for_the_blind_with_three_strips", {
 	description = "Look at the name of this node...",
 	tiles = {"black.png"},
 	drawtype = "nodebox",
@@ -33,6 +33,24 @@ minetest.register_node("apbmod:guide_strips_for_the_blind", {
 			{-0.4, -0.5, -0.4, -0.25, -0.45, 0.4},
 			{-0.075, -0.5, -0.4, 0.075, -0.45, 0.4},
 			{0.25, -0.5, -0.4, 0.4, -0.45, 0.4},
+		},
+	},
+	groups = { cracky = 3},
+	on_place = minetest.rotate_node
+})
+
+minetest.register_node("apbmod:guide_strips_for_the_blind_with_two_strips", {
+	description = "Look at the name of this node...",
+	tiles = {"black.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.5, -0.4, -0.08, -0.45, 0.4},
+			{0.08, -0.5, -0.4, 0.25, -0.45, 0.4},
 		},
 	},
 	groups = { cracky = 3},
